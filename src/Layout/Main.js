f2cc324ff3f4f53a6f7fcec38dd7917c086ef2dc
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-10-26 12:49:00 +0600

    Navbar Added

---
+++ src/Layout/Main.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+import Footer from '../Shared/Footer/Footer';
+import Header from '../Shared/Header/Header';
+import LeftsideNav from '../Shared/LeftsideNav/LeftsideNav';
+import RightsideNav from '../Shared/RightsideNav/RightsideNav';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+           
+            
+               
+           
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlnYkmX8lFv9xdqBzK91h5bXsMrPNqRm8",
+  authDomain: "learn-code-b31de.firebaseapp.com",
+  projectId: "learn-code-b31de",
+  storageBucket: "learn-code-b31de.appspot.com",
+  messagingSenderId: "200026182218",
+  appId: "1:200026182218:web:ef6ea1e9dce6dff2214cb3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
