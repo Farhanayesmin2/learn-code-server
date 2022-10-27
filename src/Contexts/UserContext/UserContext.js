96d330b323488af80f7b2e31898bf8012d22dde6
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-10-27 23:42:55 +0600

    Register connect with firebase

--- src/Contexts/UserContext/UserContext.js
+++ src/Contexts/UserContext/UserContext.js
@@ -1,12 +1,11 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
-import app from '../../firebase/firebase.config';
-
+import app from '../../firebase/firebase.config'
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-const AuthProvider = ({ children }) => {
+const UserContext = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -20,7 +19,7 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const signIn = (email, password) => {
+    const logIn = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
@@ -63,7 +62,7 @@ const AuthProvider = ({ children }) => {
         updateUserProfile,
         verifyEmail,
         createUser, 
-        signIn 
+        logIn 
     };
 
     return (
@@ -73,4 +72,4 @@ const AuthProvider = ({ children }) => {
     );
 };
 
-export default AuthProvider;
+export default UserContext;
--- src/RegisterForm/Register/Register.js
+++ src/RegisterForm/Register/Register.js
@@ -1,13 +1,82 @@
-import React from 'react';
+import {  Alert, Toast } from 'flowbite-react';
+import React, { useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/UserContext/UserContext';
+
 
 const Register = () => {
+  const [error, setError] = useState('');
+ 
+  const [accepted, setAccepted] = useState(false);
+  const { createUser, updateUserProfile, verifyEmail } = useContext(AuthContext);
+
+  const handleRegister = event => {
+      event.preventDefault();
+      const form = event.target;
+      const name = form.name.value;
+      const photoURL = form.photoURL.value;
+      const email = form.email.value;
+      const password = form.password.value;
+      const confirm = form.confirm.value;
+      console.log(name, photoURL, email, password);
+
+      createUser(email, password)
+          .then(result => {
+              const user = result.user;
+              console.log(user);
+              setError('');
+              form.reset();
+              handleUpdateUserProfile(name, photoURL);
+              handleEmailVerification();
+              //.success('Please verify your email address.')
+          })
+          .catch(e => {
+              console.error(e);
+              setError(e.message);
+          });
+    
+    
+          if (password.length < 6) {
+            setError("Password Should be 6 character or more.")
+        }
+        if (password !== confirm) {
+            setError("Your Password did not match.");
+            return;
+        }
+    
+    
+    
+  }
+
+  const handleUpdateUserProfile = (name, photoURL) => {
+      const profile = {
+          displayName: name,
+          photoURL: photoURL
+      }
+
+      updateUserProfile(profile)
+          .then(() => { })
+          .catch(error => console.error(error));
+  }
+
+  const handleEmailVerification  = () => {
+      verifyEmail()
+      .then(() =>{})
+      .catch(error => console.error(error));
+  }
+
+  const handleAccepted = event => {
+      setAccepted(event.target.checked)
+  }
+
+
     return (
         <div>
              <div className='font-sans '> 
-          
-          <div className="shadow-2xl bg-gradient-to-tr  from-teal-200 via-gray-200 to-violet-200    p-4 w-full max-w-sm  rounded-lg border border-rose-400  sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-<form className="space-y-6" action="#">
+             <section className=" min-h-screen flex items-center justify-center">
+             <div className="shadow-2xl bg-gradient-to-tr  from-teal-200 via-gray-200 to-violet-200    p-4 w-full max-w-sm  rounded-lg border border-rose-400  sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+<form onSubmit={handleRegister}  className="space-y-6" >
   <h5 className="text-2xl text-center font-bold text-gray-900 dark:text-white ">
   Sign Up for more Update
   </h5>
@@ -23,12 +92,21 @@ const Register = () => {
       type="name"
       name="name"
                  
-                  value="name"
+                
       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
       placeholder="name"
       required=""
     />
-  </div>        
+  </div>   
+              <div>
+                
+<label class="block mb-2 text-sm font-medium text-green-900 dark:text-gray-300" for="file_input">Upload file</label>
+                <input
+                  name="photoURL"
+                  type="file"
+                  class="block  w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"   />
+
+              </div>     
 
   <div>
     <label
@@ -40,9 +118,9 @@ const Register = () => {
     <input
       type="email"
       name="email"
-     value="email"
+   
       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-      placeholder="name"
+      placeholder="email"
       required=""
     />
   </div>
@@ -56,8 +134,24 @@ const Register = () => {
     <input
       type="password"
       name="password"
-      value="password"
-      placeholder="••••••••"
+     
+      placeholder="Password"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+      required=""
+    />
+  </div>
+  <div>
+    <label
+      htmlFor="password"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+    >
+      Confirm password
+    </label>
+    <input
+      type="password"
+      name="confirm"
+     
+      placeholder="Confirm Password"
       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
       required=""
     />
@@ -67,7 +161,9 @@ const Register = () => {
       <div className="flex items-center h-5">
         
       </div>
-     
+      <p className="text-danger">
+                {error}
+            </p>
     </div>
     <Link
     
@@ -83,13 +179,160 @@ const Register = () => {
     Login to your account
   </button>
   <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-    Have not Account?{" "}
-    <Link className="text-blue-700 hover:underline dark:text-blue-500">
-     Register
+   Already Have an Account?{" "}
+    <Link to="/login" className="text-blue-700 hover:underline dark:text-blue-500">
+    Login
     </Link>
   </div>
 </form>
 </div>
+    {/* image */}
+    <div className="lg:block hidden w-1/2">
+      <img alt='login-img'
+        className="rounded-3xl  "
+        src="https://fgstardustresort.com/wp-content/uploads/2021/04/signin-image.jpg"
+      />
+    </div>
+ 
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+{/*           
+          <div className="shadow-2xl bg-gradient-to-tr  from-teal-200 via-gray-200 to-violet-200    p-4 w-full max-w-sm  rounded-lg border border-rose-400  sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+<form onSubmit={handleRegister}  className="space-y-6" >
+  <h5 className="text-2xl text-center font-bold text-gray-900 dark:text-white ">
+  Sign Up for more Update
+  </h5>
+
+  <div>
+    <label
+      htmlFor="name"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+    >
+      Your name
+    </label>
+    <input
+      type="name"
+      name="name"
+                 
+                
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+      placeholder="name"
+      required=""
+    />
+  </div>   
+              <div>
+                
+<label class="block mb-2 text-sm font-medium text-green-900 dark:text-gray-300" for="file_input">Upload file</label>
+                <input
+                  name="photoURL"
+                  type="file"
+                  class="block  w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"   />
+
+              </div>     
+
+  <div>
+    <label
+      htmlFor="email"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+    >
+      Your email
+    </label>
+    <input
+      type="email"
+      name="email"
+   
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+      placeholder="email"
+      required=""
+    />
+  </div>
+  <div>
+    <label
+      htmlFor="password"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+    >
+      Your password
+    </label>
+    <input
+      type="password"
+      name="password"
+     
+      placeholder="Password"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+      required=""
+    />
+  </div>
+  <div>
+    <label
+      htmlFor="password"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+    >
+      Confirm password
+    </label>
+    <input
+      type="password"
+      name="confirm"
+     
+      placeholder="Confirm Password"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+      required=""
+    />
+  </div>
+  <div className="flex items-start">
+    <div className="flex items-start">
+      <div className="flex items-center h-5">
+        
+      </div>
+      <p className="text-danger">
+                {error}
+            </p>
+    </div>
+    <Link
+    
+      className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+    >
+      Forget Password?
+    </Link>
+  </div>
+  <button
+    type="submit"
+    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    Login to your account
+  </button>
+  <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+   Already Have an Account?{" "}
+    <Link to="/login" className="text-blue-700 hover:underline dark:text-blue-500">
+    Login
+    </Link>
+  </div>
+</form>
+</div> */}
 </div>
         </div>
     );
